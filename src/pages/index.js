e9c93e5e4ee324de4fbfff57e71b98aa25d67b7d
Mariusz Wachowski <wachowski.mariusz@gmail.com>
2019-10-29 20:59:00 +0100

    added email input validation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from "../components/layout";
 import Timer from "../components/timer"
+import EmailForm from '../components/EmailForm';
 import Particles from 'react-particles-js';
 
 const particlesOptions = {
@@ -16,12 +17,10 @@ const particlesOptions = {
 };
 
 class IndexPage extends React.Component {
-  state={
-    email: ''
+  handleEmailSubmit = (email) => {
+    // do sth with email
   }
-  handleV = (e) => {
-    this.setState({ email: e.target.value })
-  }
+
   render() {
     return(
       <Layout>
@@ -33,8 +32,7 @@ class IndexPage extends React.Component {
                 <h1 className="h1">COMING SOON</h1>
                 <Timer />
                 <p>We will be launch soon, enter your email address and get our early notification.</p>
-                <input name="email" type="text" placeholder="email address" />
-                <button className="button">Submit</button>
+                <EmailForm onSubmit={this.handleEmailSubmit} />
                 <div id="mc_embed_signup">
               </div>
                 <div className="btn-group">
